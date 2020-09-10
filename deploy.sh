e4f783c96fa53ca50a784235fb9817be80ab5af4
alejomontoya <montoyaa241@gmail.com>
2020-09-10 07:59:53 -0500

    fix deploy file

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/alejomontoya/ricky-and-morty-vue-app.git master:gh-pages
+git push -f https://github.com/alejomontoya/rick-and-morty-vue-app.git master:gh-pages
 
 
 cd -
